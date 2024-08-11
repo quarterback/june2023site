a913d3210f5d089b9721ac7c50baf20f969be3ee
Ron Bronson <contact@ronbronson.com>
2024-08-10 19:08:52 -0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        skin: {
-          fill: 'rgb(var(--color-background) / <alpha-value>)',
-          base: 'rgb(var(--color-text) / <alpha-value>)',
-          accent: 'rgb(var(--color-accent) / <alpha-value>)',
-          inverted: 'rgb(var(--color-background) / <alpha-value>)',
-          primary: 'rgb(var(--color-primary) / <alpha-value>)',
-          secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
-        },
-      },
-      fontFamily: {
-        sans: ['Ultra', 'sans-serif'],
-        serif: ['Slabo 27px', 'serif'],
-      },
+      // Your existing theme extensions...
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    // Any other plugins you're using...
+  ],
 };
